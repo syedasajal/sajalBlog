6720c8478bd9ba6a4db72db18d2e317bf67f6682
syedasajal <syedasajal8@gmail.com>
2022-08-27 11:35:41 +0500

    user profile added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -7,6 +8,26 @@ def home(request):
         'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'   # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request,'blog/about.html', {'title': 'About'})
